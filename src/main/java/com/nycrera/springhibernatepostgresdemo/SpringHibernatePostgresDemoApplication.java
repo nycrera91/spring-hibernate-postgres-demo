2fc60467d13192d424b0b7bd91f8ec83cb02fe5e
nycrera91 <nycrera@gmail.com>
2020-05-18 14:49:18 +0300

    Initial commit.

---
+++ src/main/java/com/nycrera/springhibernatepostgresdemo/SpringHibernatePostgresDemoApplication.java
@@ -0,0 +1,13 @@
+package com.nycrera.springhibernatepostgresdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernatePostgresDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHibernatePostgresDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nycrera/springhibernatepostgresdemo/SpringHibernatePostgresDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nycrera.springhibernatepostgresdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHibernatePostgresDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
