cefa3b017e9717dd5ce40e0f7ecb9894943e2dd9
nycrera91 <nycrera@gmail.com>
2020-05-23 10:15:37 +0300

    Endpoints added for Answers.

--- src/main/java/com/nycrera/springhibernatepostgresdemo/controller/AnswerQuestion.java
+++ src/main/java/com/nycrera/springhibernatepostgresdemo/controller/AnswerQuestion.java
@@ -5,6 +5,7 @@ import com.nycrera.springhibernatepostgresdemo.model.Answer;
 import com.nycrera.springhibernatepostgresdemo.repository.AnswerRepository;
 import com.nycrera.springhibernatepostgresdemo.repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -42,6 +43,34 @@ public class AnswerQuestion {
 
     }
 
+    @RequestMapping(value = "/questions/{questionId}/answers/{answerId}", method = RequestMethod.PUT)
+    public Answer updateAnswer(@PathVariable Long questionId, @PathVariable Long answerId, @RequestBody Answer answerRequest) {
+
+        if(!questionRepository.existsById(questionId)) {
+            throw  new ResourceNotFound("Question not found with id: " + questionId);
+        }
+
+        return answerRepository.findById(answerId)
+                    .map(answer -> {
+                        answer.setText(answerRequest.getText());
+                        return answerRepository.save(answer);
+                    }).orElseThrow(() -> new ResourceNotFound("Answer not found with id: " + answerId));
+    }
+
+    @RequestMapping(value = "/questions/{questionId}/answers/{answerId}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> deleteAnswer(@PathVariable Long questionId, @PathVariable Long answerId) {
+
+        if(!questionRepository.existsById(questionId)) {
+            throw new ResourceNotFound("Question not found with id: " + questionId);
+        }
+
+        return answerRepository.findById(answerId)
+                .map(answer -> {
+                    answerRepository.delete(answer);
+                    return ResponseEntity.ok().build();
+                }).orElseThrow(() -> new ResourceNotFound("Answer not found with id: " + answerId));
+
+    }
 
 
 }
